f9bdf8a981b56fd5c1b566948f94e7d50d55d420
jeff <knifed@gmail.com>
2013-11-23 20:55:25 +0000

    basic stuff

---
+++ java/src/is/hi/sdm/gutenberg/PreprocessorHelp.java
@@ -0,0 +1,7 @@
+package is.hi.sdm.gutenberg;
+
+public class PreprocessorHelp {
+	public static void main(String[] args) {
+		System.out.println("This jar has to be run through hadoop, not the command line.");
+	}
+}
