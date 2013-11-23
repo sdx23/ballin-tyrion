a04da600f3ddb6e4712a274b9469af330bb7b98b
jeff <knifed@gmail.com>
2013-11-23 21:04:45 +0000

    get it to build the jar, and only do compile when building through eclipse

--- java/src/is/hi/sdm/gutenberg/PreprocessorHelp.java
+++ java/src/is/hi/sdm/gutenberg/PreprocessorHelp.java
@@ -2,6 +2,6 @@ package is.hi.sdm.gutenberg;
 
 public class PreprocessorHelp {
 	public static void main(String[] args) {
-		System.out.println("This jar has to be run through hadoop, not the command line.");
+		System.out.println("This jar has to be run through hadoop, not the command linee.");
 	}
 }
