20a9d2dfcffc2813d143ca783cd36bbc22ce037e
jeff <knifed@gmail.com>
2013-11-23 22:31:16 +0000

    fix issues with 1.6 compliance level, add job script

---
+++ java/pbs-example.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#PBS -q batch
+#PBS -N gutenberg-preprocessor
+#PBS -l nodes=2:ppn=4
+#PBS -o hadoop_run.out
+#PBS -e hadoop_run.err
+#PBS -V
+
+module add java
+
+### Run the myHadoop environment script to set the appropriate variables
+#
+# Note: ensure that the variables are set correctly in bin/setenv.sh
+. /N/soft/myHadoop/bin/setenv.sh
+
+#### Set this to the directory where Hadoop configs should be generated
+# Don't change the name of this variable (HADOOP_CONF_DIR) as it is
+# required by Hadoop - all config files will be picked up from here
+#
+# Make sure that this is accessible to all nodes
+export HADOOP_CONF_DIR="${HOME}/gutenberg-preprocessor-config"
+
+#### Set up the configuration
+# Make sure number of nodes is the same as what you have requested from PBS
+# usage: $MY_HADOOP_HOME/bin/pbs-configure.sh -h
+echo "Set up the configurations for myHadoop"
+# this is the non-persistent mode
+$MY_HADOOP_HOME/bin/pbs-configure.sh -n 4 -c $HADOOP_CONF_DIR
+# this is the persistent mode
+# $MY_HADOOP_HOME/bin/pbs-configure.sh -n 4 -c $HADOOP_CONF_DIR -p -d /oasis/cloudstor-group/HDFS
+echo
+
+#### Format HDFS, if this is the first time or not a persistent instance
+echo "Format HDFS"
+$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR namenode -format
+echo
+
+#### Start the Hadoop cluster
+echo "Start all Hadoop daemons"
+$HADOOP_HOME/bin/start-all.sh
+#$HADOOP_HOME/bin/hadoop dfsadmin -safemode leave
+echo
+
+#### Run your jobs here
+echo "Preprocessing Gutenberg"
+$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR dfs -mkdir gutenberg
+$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR dfs -mkdir gbp-outputs
+$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR dfs -copyFromLocal ~/dist/*.zip gutenberg
+$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR dfs -ls gutenberg
+$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR jar ~/dist/gutenberg-preprocessor.jar gutenberg/*.zip gbp-outputs
+$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR dfs -ls gbp-outputs
+$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR dfs -copyToLocal gbp-outputs ${HOME}/gutenberg-preprocessor-outputs
+echo
+
+#### Stop the Hadoop cluster
+echo "Stop all Hadoop daemons"
+$HADOOP_HOME/bin/stop-all.sh
+echo
+
+#### Clean up the working directories after job completion
+echo "Clean up"
+$MY_HADOOP_HOME/bin/pbs-cleanup.sh -n 4 -c $HADOOP_CONF_DIR
+echo
