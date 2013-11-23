010341bd0342933ed168c52c8ea50375ac70d89d
jeff <knifed@gmail.com>
2013-11-23 23:16:32 +0000

    rework job script somewhat

--- java/pbs-example.sh
+++ java/pbs-example.sh
@@ -44,11 +44,10 @@ echo
 
 #### Run your jobs here
 echo "Preprocessing Gutenberg"
-$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR dfs -mkdir gutenberg
-$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR dfs -mkdir gbp-outputs
-$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR dfs -copyFromLocal ~/dist/*.zip gutenberg
-$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR dfs -ls gutenberg
-$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR jar ~/dist/gutenberg-preprocessor.jar gutenberg/*.zip gbp-outputs
+$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR dfs -mkdir gbp-inputs
+$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR dfs -copyFromLocal ~/dist/*.zip gbp-inputs
+$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR dfs -ls gbp-inputs
+$HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR jar ~/dist/gutenberg-preprocessor.jar gbp-inputs/*.zip gbp-outputs
 $HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR dfs -ls gbp-outputs
 $HADOOP_HOME/bin/hadoop --config $HADOOP_CONF_DIR dfs -copyToLocal gbp-outputs ${HOME}/gutenberg-preprocessor-outputs
 echo
