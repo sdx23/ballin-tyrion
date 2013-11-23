9f10337517a119dca7a930ff5ae8005a64f3b126
jeff <knifed@gmail.com>
2013-11-23 22:54:36 +0000

    remove the help class and switch to the actual hadoop main class

--- java/src/is/hi/sdm/gutenberg/PreprocessorHelp.java
+++
@@ -1,7 +0,0 @@
-package is.hi.sdm.gutenberg;
-
-public class PreprocessorHelp {
-	public static void main(String[] args) {
-		System.out.println("This jar has to be run through hadoop, not the command linee.");
-	}
-}
