0aaac2aa35fdec6357179dcf55aa26e8a345a107
Max Voit <max.voit+gtdv@with-eyes.net>
2013-11-24 18:04:49 +0000

    downloader supports directory argument

--- subjects/download.sh
+++ subjects/download.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+downloaddir="$1"
+
 while read line
 do
 	# get id and download the zip
     id=${line%%	*}
     echo "id $id"
-	wget "http://www.gutenberg.org/files/$id/$id.zip"
+	wget -P "$downloaddir" "http://www.gutenberg.org/files/$id/$id.zip"
 	# check number of file inside
 	nf=$( unzip -l $id.zip | tail -1 | awk '{ print $2 }' )
 	[ $nf == 1 ] || rm -f $id.zip
