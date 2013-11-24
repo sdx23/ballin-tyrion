0f784a6211ed24450c3697c7baf8bd79a6990cfa
Max Voit <max.voit+gtdv@with-eyes.net>
2013-11-24 18:11:17 +0000

    fixed downloader number of files check

--- subjects/download.sh
+++ subjects/download.sh
@@ -9,6 +9,6 @@ do
     echo "id $id"
 	wget -P "$downloaddir" "http://www.gutenberg.org/files/$id/$id.zip"
 	# check number of file inside
-	nf=$( unzip -l $id.zip | tail -1 | awk '{ print $2 }' )
-	[ $nf == 1 ] || rm -f $id.zip
+	nf=$( unzip -l ${downloaddir}$id.zip | tail -1 | awk '{ print $2 }' )
+	[ $nf == 1 ] || rm -f ${downloaddir}$id.zip
 done < subjects.dat
